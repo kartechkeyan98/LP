3bd2de47c250a1500dae78282b4d1492d0a1a81f
Kartikeyan Iyer <kktalcore02@gmail.com>
2026-03-10 18:37:48 -0700

    Refactoring for modularity

--- res/lp/include/utils/concepts.hpp
+++ res/lp/include/utils/concepts.hpp
@@ -42,48 +42,6 @@ concept ord_field= field<T> && requires (T a, T b){
 template<typename T>
 concept number = std::integral<T> || std::floating_point<T>;
 
-// Core algebraic operations concept (meant to detect scalars only)
-template<typename T>
-concept algebraic = requires(T a, const T& b) {
-    // Basic arithmetic operations
-    { a + b } -> std::convertible_to<T>;
-    { a - b } -> std::convertible_to<T>;
-    { a * b } -> std::convertible_to<T>;
-    { a / b } -> std::convertible_to<T>;
-    
-    // Assignment versions
-    { a += b } -> std::convertible_to<T>;
-    { a -= b } -> std::convertible_to<T>;
-    { a *= b } -> std::convertible_to<T>;
-    { a /= b } -> std::convertible_to<T>;
-    
-    // Unary operations
-    { +a } -> std::convertible_to<T>;
-    { -a } -> std::convertible_to<T>;
-    
-    // Comparison (often needed for processing)
-    { a == b } -> std::convertible_to<bool>;
-    { a != b } -> std::convertible_to<bool>;
-    
-    // Construction and assignment
-    requires std::is_default_constructible_v<T>;
-    requires std::is_copy_constructible_v<T>;
-    requires std::is_copy_assignable_v<T>;
-    requires std::is_move_constructible_v<T>;
-    requires std::is_move_assignable_v<T>;
-};
-
-template<typename T>
-concept ord_algebraic= algebraic<T> && requires(const T& a, const T& b){
-    { a < b } -> std::convertible_to<bool>;
-    { a > b } -> std::convertible_to<bool>;
-    { a <=b } -> std::convertible_to<bool>;
-    { a >=b } -> std::convertible_to<bool>;
-};
-
-
-
-
 
 }
 
